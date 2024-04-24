ab42009930caf2ea9bfa805fde80db3b4ff45c01
Aatif Syed <aatifsyedyp@gmail.com>
2024-04-25 01:26:41 +0200

    chore: partial_eq.rs compiles against std

--- tests/partial_eq.rs
+++ tests/partial_eq.rs
@@ -1,3 +1,10 @@
+#![allow(path_statements, clippy::no_effect)]
+
+use std::{borrow::Cow, collections::VecDeque};
+
+type Slice<T> = [T];
+type Array<const N: usize, T> = [T; N];
+
 const _: () = {
     fn _test<A, B>()
     where
@@ -89,142 +96,127 @@ const _: () = {
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <VecDeque<T, A> as PartialEq<&Slice<U>>>::eq;
+        <VecDeque<T> as PartialEq<&Slice<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Vec<T, A> as PartialEq<&Slice<U>>>::eq;
+        <Vec<T> as PartialEq<&Slice<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <VecDeque<T, A> as PartialEq<&mut Slice<U>>>::eq;
+        <VecDeque<T> as PartialEq<&mut Slice<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Vec<T, A> as PartialEq<&mut Slice<U>>>::eq;
+        <Vec<T> as PartialEq<&mut Slice<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Vec<T, A> as PartialEq<Slice<U>>>::eq;
+        <Vec<T> as PartialEq<Slice<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <&Slice<T> as PartialEq<Vec<U, A>>>::eq;
+        <&Slice<T> as PartialEq<Vec<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <&mut Slice<T> as PartialEq<Vec<U, A>>>::eq;
+        <&mut Slice<T> as PartialEq<Vec<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U> + Clone,
     {
-        <Cow<'_, Slice<T>> as PartialEq<Vec<U, A>>>::eq;
+        <Cow<'_, Slice<T>> as PartialEq<Vec<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A>()
+    fn _test<T, U>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Slice<T> as PartialEq<Vec<U, A>>>::eq;
+        <Slice<T> as PartialEq<Vec<U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A, const N: usize>()
+    fn _test<T, U, const N: usize>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <VecDeque<T, A> as PartialEq<&Array<N, U>>>::eq;
+        <VecDeque<T> as PartialEq<&Array<N, U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A, const N: usize>()
+    fn _test<T, U, const N: usize>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Vec<T, A> as PartialEq<&Array<N, U>>>::eq;
+        <Vec<T> as PartialEq<&Array<N, U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A, const N: usize>()
+    fn _test<T, U, const N: usize>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <VecDeque<T, A> as PartialEq<&mut Array<N, U>>>::eq;
+        <VecDeque<T> as PartialEq<&mut Array<N, U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A, const N: usize>()
+    fn _test<T, U, const N: usize>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <VecDeque<T, A> as PartialEq<Array<N, U>>>::eq;
+        <VecDeque<T> as PartialEq<Array<N, U>>>::eq;
     }
 };
 
 const _: () = {
-    fn _test<T, U, A, const N: usize>()
+    fn _test<T, U, const N: usize>()
     where
-        A: Allocator,
         T: PartialEq<U>,
     {
-        <Vec<T, A> as PartialEq<Array<N, U>>>::eq;
+        <Vec<T> as PartialEq<Array<N, U>>>::eq;
     }
 };
-
